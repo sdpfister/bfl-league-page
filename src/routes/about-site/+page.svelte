ee7ca64e586edb55112680bd3a48874587676a11
Shane <sdpfister@gmail.com>
2023-08-26 21:39:16 -0400

    Added Roadmap link to About Site modified:   src/routes/about-site/+page.svelte

--- src/routes/about-site/+page.svelte
+++ src/routes/about-site/+page.svelte
@@ -14,6 +14,13 @@
         margin: 2em 0 1.5em;
     }
 
+    h4 {
+        text-decoration: underline;
+        margin-left: 2em;
+        font-size: 1.2em;
+        line-height: 1.2em;
+    }
+
     .subBlock {
         margin-left: 2.4em;
     }
@@ -34,5 +41,11 @@
         <p>We'll be working hard to make this a premier fantasy football league site and a one-stop shop for BFL managers. Whether you're catching up on league happenings or prepping for the next verbal battle, this site will provide everything you need to dominate the competition both on and off the virtual field.</p>
             
         <p>So bookmark this site for your one-stop shop of fantasy football news, trash talk, and general heckling. And may the football gods have mercy on your soul, because we sure as hell won't!</p>
+    </div>
+
+    <h4>Site Roadmap</h4>
+
+    <div class="subBlock">
+        <p>Eager to see how this website will evolve? Take a peek at our <a href="/roadmap">Roadmap</a> for a glimpse of the enhancements and new features we're working on.</p>
     </div>
 </div>
