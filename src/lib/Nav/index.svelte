dbc915901dfa4c7285315cad1ac1bf289c66b5e1
Shane <sdpfister@gmail.com>
2023-09-02 11:28:11 -0400

    Updated all favicons, favicons code, site titling
        modified:   src/app.html
    	modified:   src/lib/Nav/index.svelte
    	new file:   static/favicons/Archive/android-chrome-192x192.png
    	new file:   static/favicons/Archive/android-chrome-512x512.png
    	new file:   static/favicons/Archive/apple-touch-icon.png
    	renamed:    static/favicons/bfl-67x67.png -> static/favicons/Archive/bfl-67x67.png
    	new file:   static/favicons/Archive/browserconfig.xml
    	new file:   static/favicons/Archive/favicon-16x16.png
    	new file:   static/favicons/Archive/favicon-32x32.png
    	new file:   static/favicons/Archive/favicon.ico
    	new file:   static/favicons/Archive/mstile-150x150.png
    	new file:   static/favicons/Archive/safari-pinned-tab.svg
    	new file:   static/favicons/README.md
    	modified:   static/favicons/android-chrome-192x192.png
    	modified:   static/favicons/android-chrome-512x512.png
    	modified:   static/favicons/apple-touch-icon.png
    	modified:   static/favicons/browserconfig.xml
    	modified:   static/favicons/favicon-16x16.png
    	modified:   static/favicons/favicon-32x32.png
    	modified:   static/favicons/favicon.ico
    	new file:   static/favicons/html_code.html
    	modified:   static/favicons/mstile-150x150.png
    	modified:   static/favicons/safari-pinned-tab.svg
    	new file:   static/favicons/site.webmanifest
    	modified:   static/manifest.json

--- src/lib/Nav/index.svelte
+++ src/lib/Nav/index.svelte
@@ -31,7 +31,7 @@
 </script>
 
 <svelte:head>
-	<title>{!$page.url.pathname[1] ? 'Home' : $page.url.pathname[1].toUpperCase() + $page.url.pathname.slice(2)} | League Page</title>
+	<title>{!$page.url.pathname[1] ? 'Home' : $page.url.pathname[1].toUpperCase() + $page.url.pathname.slice(2)} | BFL HQ</title>
 </svelte:head>
 
 <style>
